889a35c5f714b8594a3c052dad287d9756c973fb
shahadat2772 <shahadat2772@gmailcom>
2022-04-28 12:00:17 +0600

    ready to deploy

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Server initialized");
+  res.send("EmaJohn Server initialized");
 });
 
 app.listen(port, () => {
